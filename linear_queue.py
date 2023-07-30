60ec9a53da7aaa84f7aaf5ecc759710a820d89c2
Vesta Ballah <135280117+TCorvah@users.noreply.github.com>
2023-07-29 21:07:49 -0500

    making updates on code

--- linear_queue.py
+++ linear_queue.py
@@ -1,44 +1,58 @@
 # stack Module
 
-def getQueue():
-
-    """Creates and returns an empty queue"""
-    return []
-
-def isEmpty(s):
-    """Returns True if stack empty, otherwise returns False."""
-    if s == []:
-        return True
-    else:
-        return False
-
-def push(s, items):
-    """ Pushes item on top of queue"""
-    s.append(items)
-
-def qsize(s):
-    len(s)
+class Queue():
+    def __init__(self):
+        self.queue = []
+	
+    def isEmpty(self):
+        if self.queue == []:
+            print("queue is empty")
+            return True			
+        else:
+            return False
     
+    def top(self):
+        if self.isEmpty():
+            print("queue is empty")
+            return None
+        else:
+            return self.queue[len(self.queue)]
 
+    def add_queue(self, items):
+        """ Pushes item on top of queue"""
+        self.queue.append(items)
 
-def pop(s):
-    """ Returns top of queue if queue not empty. Otherwise, returns None. """
-    if isEmpty(s):
-        return None
-    else:
-        item = s.pop(0)
-        return item
-    
-import linear_queue
-#queue items gets printed twice
-myqueue =  linear_queue.getQueue()
-lis = [1 , 2, 3, 4]
-for item in lis:
-    linear_queue.push(myqueue, item)   
-    print("pushing", item, "on queue")
-while not linear_queue.isEmpty(myqueue):
-    item = linear_queue.pop(myqueue)
-    print("popping", item, "from queue")
+    def pops(self):
+        """ Returns top of queue if queue not empty. Otherwise, returns None. """
+        if self.isEmpty():
+            return None
+        else:
+            item = self.queue.pop(0)
+            return item
+myqueue = Queue()	
+def FiFo(graph):
+    src = graph[0]
+    visited = set()
+    visited.add(src)
+    myqueue.add_queue(src)
+    print("initial element on queue", src)
+    while not myqueue.isEmpty():
+        for neighbor in graph:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                myqueue.add_queue(neighbor)
+                print(" adding items on queue", neighbor)
+        current = myqueue.pops()
+        print("removing items from queue", current)
+    return "done"  
+
+if __name__ == "__main__":
+	queue= [0, 1, 2, 7]
+	x = FiFo(queue)
+	print(x)
+	
+  
+   
 
     
 
