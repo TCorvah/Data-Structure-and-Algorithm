b920bb381ccf20f36129cc77027670de82b3c57c
Vesta Ballah <135280117+TCorvah@users.noreply.github.com>
2023-08-08 17:12:24 -0500

    new code changes to file

--- AStarHeuristic.py
+++ AStarHeuristic.py
@@ -1,14 +1,8 @@
-from Graph import Edge
-import math
-class AStarHeuristics(Edge):
 
-    def getWeight(self,source, dest):
-        first = Edge(source, self.cost)
-        dest = Edge(dest, self.cost)
-        return first.cost, dest.cost
+class AStarHeuristics():   
+
+    def getWeight(source, dest):
+       return source, dest
 
 
-
-
-v = AStarHeuristics.getWeight(5, dest=5)
-print(v)
+        
