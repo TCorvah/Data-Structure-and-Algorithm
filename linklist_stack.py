9279a56997987a4346f49c8419771aabc13cf256
Vesta Ballah <135280117+TCorvah@users.noreply.github.com>
2023-08-27 20:17:33 -0500

    added new comments to linklist_stack.py

--- linklist_stack.py
+++ linklist_stack.py
@@ -1,26 +1,31 @@
 import math
 import sys
 
+#This is a singly linklist implementaion of a stack
 class Nodes():
 	def __init__(self,data):
 		self.data = data
 		self.next = None
 
-
+#The stack implementation consist of various functions
+#initially, the head of the stack is empty, as param head == None
+#a count parameter is added to keep track of stack counts. Not really needed
 class Stacks():
 	def __init__(self):
 		self.head = None
 		self.count = 0
 	
-	
 	def isEmpty(self):
+		""" Returns True if stack is empty, otherwise returns False"""
 		if self.head == None:
+			print("empty stack")
 			return True
 		else:
 			return False
 		
 	
 	def push(self, item):
+		"""Pushes item on the top of the stack"""
 		if self.isEmpty():
 			self.head = Nodes(item)
 		else:
@@ -30,12 +35,18 @@ class Stacks():
 			self.count += 1  		
 		
 	def top(self):
+		"""Returns value of the top item of stack, if stack not empty.
+			Otherwise, retuns None
+		"""
 		if self.isEmpty():
 			print("stack is empty")
 		else:
-			return self.head	
+			temp = self.head.next
+			self.count -= 1 
+			return temp	
 
 	def pop(self):
+		"""Returns top of stack if stack is empty, otherwise returns None"""
 		curr = self.head
 		if curr == None:
 			print("empty stack")
@@ -47,8 +58,8 @@ class Stacks():
 			return temp
 		
 	def getInput(self):
+		"""Returns the user input for the stack, exits if input is -1"""
 		num = int(input('Enter values for stack input (1 - num) (-1 to quit):  '))
-        #added one as the range function counts up to but not including the last element
 		while num < 1 and  num != -1:
 			if num == -1:
 				print("bye\n") 
@@ -58,7 +69,8 @@ class Stacks():
 		return num
 	
 
-
+#This driver code function creates and returns a new empty stack
+# an error check is included so that only valid inputs are enter for the stack
 def linkStack():
 	head = Stacks()
 	valid = False
@@ -73,7 +85,7 @@ def linkStack():
 					print('pushing -> : {}'.format(item), "on stack") 
 				while not head.isEmpty():
 					item = head.pop()
-					print('popping -> : {}'.format(item), "from stack") 
+					print('popping <- : {}'.format(item), "from stack") 
 		except ValueError as err_msg:
 			print(err_msg,'\n') 
 
