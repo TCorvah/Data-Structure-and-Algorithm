cadbbe744e84372700f91a1075093729c7ff8fd4
Vesta Ballah <135280117+TCorvah@users.noreply.github.com>
2023-10-16 15:48:08 -0400

    added working functionality to rod cut problem

--- Dynamic-Programming/rod_cutting_problem.py
+++ Dynamic-Programming/rod_cutting_problem.py
@@ -1,4 +1,5 @@
 import math
+import sys
 """_summary_ The following uses dynamic programming to solve the rod-cutting
     problem. The code utilizes a bottom up method that enforces ordering
 """
@@ -13,10 +14,10 @@ def rod_cut(price, m):
     """    
     revenue = [0 for i in range(m + 1)]
     revenue[0] = 0
-    for j in range(1,m + 1): 
-        q = 0   
+    for j in range(m + 1): 
+        q = 0 
         for i in range(j):
-            q = max(q, (price[i] + revenue[j - i]))
+            q = max(q, (price[i] + revenue[j - i - 1]))
         revenue[j] = q
     return revenue[m]
 
@@ -26,31 +27,53 @@ v = rod_cut(price, 4)
 print(v)
 
 
+
+
+
+
 def extended_rod_cut(price,n): 
+    """! The extended bottom up implementation of the rod cutting problem 
+        computes for each size j, not only the maximum revenue of r_j, but
+        s_j, the optimal size for the first piecec to cut off.
+        the runtime of this code snippet is O(n)^2 as it iterates twice
+        for each i,j in n.
+        @param the price and length of the array n 
+        @return A list of rod sizes in an optimal decomposition of the length n
+    """    
     n = len(price) 
     if n == 0:
         return 0
-    negative_infinity = -math.inf 
-    revenue = [0 for i in range(n)]
-    size = [0 for j in range(n)]
+    revenue = [0 for i in range(n + 1)]
+    size = [0 for j in range(n + 1)]
     revenue[0] = 0
-    for j in range(n-1): 
+    for j in range(n + 1): 
         q = 0
-        for i in range(1,n):
-            if q < price[i] + revenue[j-i]:
-                q = price[i] + revenue[j - i]
-                size[j] = i
+        for i in range(j):
+            if q < price[i] + revenue[j- i - 1]:
+                q = price[i] + revenue[j - i - 1]
+                size[j] = i + 1
         revenue[j] = q
     return revenue, size
 
 
+        
 def print_cut_rodSolution(price, n):
+    """! The print function prints an optimal solution for a specific revenue 
+        @param the price and length of the array n 
+        @return None, but prints cuts of revenue  corresponding to the first optimal
+        decomposition
+    """  
+    s = [0 for j in range(n + 1)] 
     (r,s) = extended_rod_cut(price,n)
     while n > 0:
-        print(s[n])
-        n = n - s[n]
+        print(s[n], end = " ")
+        n = n - s[n]  
+ 
+       
 
 p = [1,5,8,9,10,17,17,20,24,30]
+x = print_cut_rodSolution(p,7)
+print(x)
 
 
 
