ba1a6ff31daca499e66508a3ae4ab5c9449d05d1
Vesta Ballah <135280117+TCorvah@users.noreply.github.com>
2023-12-13 20:16:56 -0500

    adding maximum subarray with updates

--- Divide-N-Conquer/maximum_subbarray.py
+++ Divide-N-Conquer/maximum_subbarray.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
 """! @brief A divide and conquer algorithm
 The maximum subarray problem uses a divide and conqueur technique.
-To find a maximum subarray in an array, we divide the subarray into two subarray,
-with the claim that the maximum subarray must lie in the left half of the array,
-the right half of the array or the point that crosses divide the leftv and right half.
-The conquer step recursively solves two subproblem on len(n/2) of maximum sum. 
-the indices lies in either the left or right half or the mid point crossing.
+To find a maximum subarray in an array, one of the follwing has to happen,
+the maximum subarray is either in the subarray A[low..mid], so that low<=i<=j<=mid,
+the maximum subarray is either in the subarray A[mid+1..high], so that mid<i<=j<=high,
+the maximum subarray is either in the subarray A[low..high], which crosses the mnid point
 """
 
 ##
 #  @mainpage  subroutine max_crossing subarray,takes an input array, with indices of
-# low, mid and high and returns a sublist that contains the maximum array and the sum. 
+# low, mid and high and returns a sublist that contains the maximum subarray that crosses the midpoint. 
 # this implementation uses linear time O(n)
 #
-#  @section find_maximum_subarray returns a tuple that demarcates the maximum a maximum subarray
+#  @section find_maximum_subarray  returns a tuple that demarcates the maximum a maximum subarray
 #  along with the sum of values in a maximum subarray.
 #  @section notes_main runtime O(nlgn + n)
 #  
@@ -26,35 +25,36 @@ def find_maximum_subarray(A):
     find the indices that demarcates a maximum subarray
     when n = <= 1, the base case returns the list
     When n > 1, 
-     The divide part is implemented in as mid, where we take the floor 
-     of the lowest and highest index in the array.
-     We create two subarray and recursively finding the left and right maximum 
-     subarray.   
+    The divide part is implemented in as mid, where we take the floor 
+    of the lowest and highest index in the array.
+    We create two subarray and recursively finding the left and right maximum 
+    subarray.   
     @param A  the list containing the array to be divided.
 
     @return  a maximum subarray  with indices of the maxim sub in a array.
     """
-          
     if len(A) <= 1:
         return A
-    low = 0
-    high = len(A)
-    mid = int(math.floor(low + high)/2) 
-    sub1 = A[0:mid]
-    sub2 = A[mid:len(A)]
-    cross_mid = A[low:high//2]
-    left =  find_maximum_subarray(sub1)
-    right = find_maximum_subarray(sub2) 
-    cross =  find_max_crossing_subarray(A, low, mid, high) 
-    left_sum  = sum(sub1)
-    righ_sum = sum(sub2)
-    cross_sum = sum(cross_mid)
-    if left_sum >= righ_sum and left_sum >= cross_sum:
-        return (left, left_sum)
-    elif righ_sum >= left_sum and righ_sum >= cross_sum:
-        return (right, righ_sum)
     else:
-        return (cross, cross_sum)
+        low = 0
+        high = len(A) 
+        mid = high//2  
+        left_array = A[low:mid] 
+        right_array = A[mid+1:high] 
+        cross_array = A[low:high]
+        left = find_maximum_subarray(left_array)  
+        right = find_maximum_subarray(right_array)      
+        cross = find_max_crossing_subarray(A, low, mid-1, high-1)
+        left_sum = sum(left_array)
+        right_sum = sum(right_array)
+        cross_sum = sum(cross_array)
+        if left_sum >= right_sum and left_sum >= cross_sum:
+            return (left)
+        elif right_sum >= left_sum and right_sum >= cross_sum:
+            return (right)
+        else:
+            return (cross)
+    
     
 
 # @file find_max_crossing_subarray.py
@@ -72,28 +72,27 @@ def find_max_crossing_subarray(A, low, mid, high):
         _type_: _description_ A subarray that contains the maximum subarray
                 with the sum of the maximum elements.
     """
-    left_sum = -1
+    left_sum = 0
     sum = 0
     max_left = mid - low 
-    for i in range(max_left, low):
+    for i in range(mid, low):
         sum += A[i]
         if sum  > left_sum:
-            left_sum = sum
+            left_sum = sum 
             max_left = i 
-    right_sum = -1
-    sum = 1
-    max_right = max_left
-    for  j in range(max_left, high):
+    right_sum = 1
+    sum = 0
+    max_right = mid + 1 - high
+    for  j in range(mid, high):
         sum += A[j]
         if sum > right_sum:
             right_sum = sum
             max_right = j 
-    return (A[max_left: max_right + 1], left_sum + right_sum) 
+    return (max_left, max_right, left_sum+right_sum)
 
 
 
 
 A = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7]
-high = len(A)
 x = find_maximum_subarray(A)
 print(x)
