b2848f49d72dca291c13c3ab831893ccfe63312e
Vesta Ballah <135280117+TCorvah@users.noreply.github.com>
2023-12-22 13:33:03 -0500

    added comments to strassen algorithm

--- Divide-N-Conquer/strassen_algorithm.py
+++ Divide-N-Conquer/strassen_algorithm.py
@@ -1,45 +1,41 @@
+#!/usr/bin/env python3
+"""! @brief Strassen Matrix multiplication using Divide and Conquer
+Strassen devise a stragety to limit the number of multiplication of
+matrices from 8 to 7,  by carefully choosing columns , rows to add or subtract.
+7 recursive calls are made for the dot product and result added to the return C matrix
+
+Inputs are two matrices with even lengths, and return a runtime of O(N^log2^7)
+"""
 import math
 import numpy as np
 
-        
-def strassen_matrix_multiplication(a,b):   
+
+ # @file  strassen_matrix_multiplication.py   
+def strassen_matrix_multiplication(a,b): 
+    """! partition each matrix by blocks of 4 and uses 
+    7 mathematical operation instead of 8.
+    @param two 2^n matrices 
+    @return a block matrice C, with a runtime of 0(N^2.8)
+    """  
     n = a.shape[0]
     if n == 1:
         return np.dot(a, b)
-    
+     
+    #a matrice partition into 4 
     row, col = a.shape
-    x,y = row//2, col//2
-    #assert len(Amatrix) == 2
-    #assert len(Bmatrix) == 2
-    #assert Amatrix[1] == Bmatrix[0]  
-    
-
-    #a matrice partition into 4 rows
-
+    x,y = row//2, col//2  
     a11  = a[:x,:y]
     a12 =  a[:x, y:]
     a21 =  a[x:, :y]
     a22 =  a[x:, y:]
     
-     
+    #b matrice partition into 4 
     brow, bcol = b.shape
     c,w = brow//2, bcol//2
-    #b matrice partition into 4 columns
-    b11 =  b[:c, :w]
-    
+    b11 =  b[:c, :w]  
     b12 =  b[:c, w:]
     b21 =  b[c:, :w]
     b22 =  b[c:, w:]
-
-    
-    C = np.zeros((a.shape[0], b.shape[0]), dtype=int)
-    crow, ccol = C.shape
-    g,p = crow//2, ccol//2
-    c11 =  C[:g, :p]
-    c12 =  C[:g, p:]
-    c21 =  C[g:, :g]
-    c22 =  C[g:, g:]
-   
     
     # matrice product of a,b 
     p1 = strassen_matrix_multiplication(np.add(a11,a22),np.add(b11,b22))
@@ -50,9 +46,7 @@ def strassen_matrix_multiplication(a,b):
     p6 = strassen_matrix_multiplication(np.subtract(a21,a11),np.add(b11,b12))
     p7 = strassen_matrix_multiplication(np.subtract(a12,a22), np.add(b21,b22))
     
-   
-     # blocks of c matrix
-
+    # blocks of c matrix
     c11 = p1 + p4 - p5 + p7
     c12 = p3 + p5
     c21  = p2 + p4
