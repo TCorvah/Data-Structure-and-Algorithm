e1005ec52f545151fd721a3402de40ac23790b01
Vesta Ballah <135280117+TCorvah@users.noreply.github.com>
2023-12-22 22:24:30 -0500

    adding code changes to permute_by_sorting.py

--- Randomize_Algorithms/permute_by_sorting.py
+++ Randomize_Algorithms/permute_by_sorting.py
@@ -1,16 +1,28 @@
-from random import randrange
+#!/usr/bin/env python3
+"""! @brief A Randomize Algorithm
+The following algorithmn randomly permute an array by mapping an array to a priority
+and returning a new list of the one to one mapping of two arrays, with one containing priorities as keys 
+and the other array as values . The procedure produces a uniform random permutaion, as each random generated numbers are
+equally likely to be a priority.
+"""
+import math
+import random
+# @file permute_by_sorting.py
 def permute_by_sorting(A):
     n = len(A)
-    p = [i for i in range(n)]
-    b = []
-    c = []
+    p = [0 for i in range(n)]
+    assert len(A) == len(p)
+    map_index = {}
+    permutaion_lst= []
     for i in range(n):
-        p[i] = randrange(0, n ** n)
-        b.append((p[i],A[i]))
-    x = sorted(b)
-    for t,j in x:
-        c.append(j)
-    return b, c
+        p[i] = random.randrange(i, n**3 - 1)
+        map_index[p[i]] = A[i]
+        
+    sorted_lst = sorted(map_index.items())
+    for i,j in sorted_lst:
+        permutaion_lst.append(j)   
+        
+    return sorted_lst, permutaion_lst
         
           
         
