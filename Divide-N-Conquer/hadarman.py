f231bb8889119d9e6169e970700d4ca6c6f75231
Vesta Ballah <135280117+TCorvah@users.noreply.github.com>
2023-12-26 10:10:54 -0500

    made changes to code

--- Divide-N-Conquer/hadarman.py
+++ Divide-N-Conquer/hadarman.py
@@ -2,9 +2,9 @@
 """! @brief Hadarman  Matrix product using Divide and Conquer
 The following is an nxn hadarman matrix using only 1 and -1
 The input is a single vector of length n, and returns an nxn matrix
-with  1s and -1 at the last row, column. For each length of the input vector,
+with  -1 at the last row, column for each length of the input vector and 1 everywhere.
 The following is a divide and conquer implementation of hadarman.  Given an input vector
-the matrices are partition in 4 equal size and return the resultamt matrix.
+the matrices are partition into 4 equal sizes.
 There are two base cases for when n is 1 or 2.
 Total runtime is O(NlogN)
 """
@@ -13,10 +13,8 @@ import numpy as np
 # @file hadarman.py
 def hadarman(v):
     """! The following is an implementation of the hadarman matrix where the elements 
-    are mapped to the list [0,1] for the rows, columns of the matrix. the input n only gives the matrix size
-    there are two recursive calls made in hadarman in 0(nlgn)
-    @param A  A single vector of length n
-
+    are mapped to the list [0,1] for the rows, columns of the matrix.
+    @param v  A single vector of length n
     @return  a hadarman matrix of size nxn
     """
     n = len(v)
@@ -28,20 +26,19 @@ def hadarman(v):
     H0 = np.array([[a1,a2],[a3,a4]])
     
     if n == 1:
-        return [1]   
+        return np.array(a1)  
     if n == 2:  
-        return H0
+       return H0
     row, col = n , n 
     C = [ [i for i in range(row)] for j in range(col)]    
     H = np.array(C)
-    row, col = n, n
     c,r = row//2, col//2 
-    h11 = H[:c, :r] 
+    h11 =  H[:c, :r] 
     h12 =  H[:c, r:] 
     h21 =  H[c:, :r] 
     h22 =  H[c:, r:] 
-    p1 = hadarman(H0)
-    p2 = hadarman(H0)
+    p1 = hadarman([a1,a2])
+    p2 = hadarman([a3,a4])
     h11 = p1
     h12 = p2
     h21 = p1
