ac6da59d7e644b27acea87dd0a2b03ad147fe696
Vesta Ballah <135280117+TCorvah@users.noreply.github.com>
2024-01-05 17:08:12 -0500

    upgrading to doubly linklist

--- breadth_first.py
+++ breadth_first.py
@@ -1,47 +1,70 @@
 from collections import defaultdict
-from nodes import Node
+class Node:
+    def __init__(self,key):    
+        self.key = key
+        self.left = None
+        self.right = None
+    
+    def createNode(self,data):
+        node = Node(data) 
+        node.key = data 
+        node.right = None
+        self.prev = node.left
+        return node
 
 # A singly linklist queue implementation using breadth first search algorithm
 # in my implementation, there is no extra pointer for rear as the next element will be
 #the rear of the front of the linklist. A single pointer is initialize for the queue class
 class Queue:
     def __init__(self):
-        self.front = None           
+        self.front = None 
+        self.tail = None 
+        self.size = 0 
+        
 
     def isEmpty(self):
         """Returns true if queue is empty, otherwise returns False"""
-        if self.front ==  None:
+        if self.size == 0:
             print("queue is empty\n")
             return True
         else:
             return False
+                
 
-   
+     
     def enqueue(self, value):
         """This function adds a new element at the back of the queuue
             if the queue is empty, then the element becomes the first queue item
-        """
-        node = Node(value)
-        if self.isEmpty():
+        """      
+        node = Node(value).createNode(value)
+        if self.front == None:
             self.front = node
+            self.size += 1
         else:
             temp = self.front
             while temp.right:
                 temp = temp.right
             temp.right = node
-               
+            self.size += 1
+            
+            
+ 
 
     def dequeue(self):
-        """ Remove the first item added in the quue and returns the data of
+        """ Remove the first item added in the queue and return the data of
             the remove queue element 
         """
-        data = self.front.key
-        temp = self.front
-        self.front = self.front.right
-        temp1 = None
-        temp = temp1
-        return data
-
+        if self.isEmpty():
+            print("queue is empty\n")
+            return None
+        else:
+            temp = self.front
+            self.front = self.front.right
+            data = temp.key    
+            temp1 = None
+            temp = temp1
+            self.size -= 1
+            return data
 
     def PrintQueue(self):
         """Prints the queue element and it nearest neighbor in the directed
@@ -56,21 +79,25 @@ class Queue:
 
 
 #A graph class using adjacency list representation. in this implementation, a directed
-# graph is being used. the grah class initialize the number of vertices, visited set and the queue class
+# graph is being used. the graph class initialize the number of vertices, visited set and the queue class
 class Graph:
     def __init__(self, numVertices):
         self.v = numVertices
         self.adjlist = defaultdict(list)
         self.visited = set() 
         self.queue = Queue()
-               
-            
+      
+
+        
     def addEdge(self, src, dest):
         #add edge from src to dest:
-        newNode = Node(dest)
+        newNode = Node(dest).createNode(dest)
         newNode.right = self.adjlist[src]
         self.adjlist[src] = newNode
-
+       
+        newNode = Node(src).createNode(src)
+        newNode.right = self.adjlist[dest]
+        self.adjlist[dest] = newNode
 
     def  printGraph(self):
         for i in range((self.v)):
@@ -81,26 +108,25 @@ class Graph:
                 temp = temp.right
             print(" \n")
 
-    def bfs(self,startVertex): 
-        node = Node(startVertex)  
-        node.parent = None    
-        self.queue.enqueue(startVertex)  
-        self.visited.add(startVertex) 
+    def bfs(self,startVertex):
+        start = Node(startVertex).createNode(startVertex)
+        start.key = startVertex
+        self.visited.add(start.key)
+        self.queue.enqueue(start.key)   
         while not self.queue.isEmpty():
-            currentVertex = self.queue.dequeue() 
-            print("visited node <- {}\n".format(currentVertex), end="") 
-            self.queue.PrintQueue()         
-            temp = self.adjlist[currentVertex]    
+            self.queue.PrintQueue()  
+            currentVertex = self.queue.dequeue()
+            print("visited node <- {}\n".format(currentVertex), end="")     
+            temp = self.adjlist[currentVertex] 
             while temp:
-                adjvertex = temp.key
-                self.queue.enqueue(adjvertex) 
-                self.queue.PrintQueue()  
+                adjvertex = temp.key 
                 if adjvertex not in self.visited:
-                    self.visited.add(adjvertex)                                     
-                temp = temp.right               
+                    self.visited.add(adjvertex)  
+                    self.queue.enqueue(adjvertex)   
+                temp = temp.right         
            
-        print()
-      
+        print(" \n")
+           
           
               
 def main():   
@@ -133,4 +159,3 @@ def main():
                  print("Invalid choice")
 if __name__ == "__main__":
     main()
-
